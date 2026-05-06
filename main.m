3ba8fb698dc86727ced536e05bce1e033f27fdf1
Andy <andy@quietcode.org>
2026-05-06 19:00:04 +0200

    Move repo

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LeoClip
+//
+//  Created by Administrator on 06.05.26.
+//  Copyright __MyCompanyName__ 2026. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
